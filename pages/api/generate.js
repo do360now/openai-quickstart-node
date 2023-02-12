3951611308c63a9ed731918d06ddd486c3841d46
Clement Machado <cm@do360now.com>
2023-02-12 13:13:03 +0100

    update

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,14 +8,14 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: generatePrompt(req.body.animal),
+    prompt: generatePrompt(req.body.text),
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `${capitalizedAnimal}`;
+function generatePrompt(text) {
+  const capitalizedtext =
+    text[0].toUpperCase() + text.slice(1).toLowerCase();
+  return `${capitalizedtext}`;
 }
